6c070f67d0142f311f4e82123af12638f69e92c9
VipulRana <vipulmrana0075@gmail.com>
2021-01-21 11:43:12 -0500

    Initial commit

---
+++ Control Statements/If_else_elseif_nested.java
@@ -0,0 +1,31 @@
+//Question:
+/* https://www.hackerrank.com/challenges/java-if-else/problem */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class If_else_elseif_nested {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if (N%2 != 0)
+            System.out.println("Weird");
+        if (N%2==0)
+        {
+            if(N>=2 && N <=5)
+                System.out.println("Not Weird");
+            if(N>=6 && N <=20)
+                System.out.println("Weird");
+            if(N>20)
+                System.out.println("Not Weird");
+        }
+        scanner.close();
+    }
+}
---
+++ src/ChangingDataType.java
@@ -0,0 +1,12 @@
+class ChangingDataType
+{
+    public static void main(String args[])
+    {
+        float f=10.5f;
+        int a=(int)f;
+        byte b= (byte)129;
+        System.out.println(f);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
